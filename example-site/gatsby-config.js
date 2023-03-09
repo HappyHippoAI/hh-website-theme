6fee2e836bef2a8d44eed0e4afe4e3129f2b541e
bitcrud <thegreatdetox@tutanota.de>
2023-03-09 10:28:49 +0530

    changed the code references based on the new folder nomenclature

--- example-site/gatsby-config.js
+++ example-site/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     plugins: [
         {
-            resolve: 'gatsby-theme-portfolio-minimal',
+            resolve: 'hh-theme',
             options: {
                 // siteUrl: "https://example.com", // Used for sitemap generation
                 manifestSettings: {
